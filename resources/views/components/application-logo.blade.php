2b8a7369d12d48f2910fa598e79446d471a6146d
Evelline Krist <elinyine@gmail.com>
2025-06-24 11:43:38 +0700

    1st commit

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,1 @@
+<img src="{{asset('image/logo-sli-horizon.png')}}">
---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
+
+Route::prefix('user')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
+    Route::get('/whoami', function (Request $request) {
+        return $request->user();
+    })->middleware('auth:api');
+});
